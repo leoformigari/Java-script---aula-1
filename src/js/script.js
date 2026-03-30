5c607a68da0c3e858173dc0954a34e38d840de4d
leoformigari <leoformigarifontes@gmail.com>
2026-03-30 09:32:35 -0300

    feat: Operadores Aritméticos e lógicos

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,42 @@
-alert("Hello World !!!")
+//Declarações e variáveis
+//var, let e const
+
+var nome="Fiap";
+console.log(nome);
+
+var nome="Teste";
+console.log(nome);
+
+if(true){
+    let apelido="Fiapinho"
+    console.log(apelido);
+}
+
+const taxaJuros=0.05;
+console.log(taxaJuros);
+// taxaJuros=1.5;
+
+var exemplo1="ola-dev-var";
+console.log(typeof exemplo1)
+let exemplo2="ola-dev-let";
+console.log(exemplo2)
+const exemplo3="ola-dev-const";
+console.log(exemplo3)
+
+let exemplo4=10;
+console.log(exemplo4)
+
+let exemplo5=true;
+console.log(exemplo5)
+
+let exemplo6={nome1:"teste"};
+console.log(exemplo6)
+
+//valor undefined
+let exemplo7;
+console.log(exemplo7)
+
+//valor vazio
+let exemplo8=null;
+console.log(exemplo8)
+
